5c9d81eac556aab384faa168d08a4ebcc891b2ce
yash264 <yp20222068@gmail.com>
2025-03-24 10:38:54 +0530

    Added Server Side

---
+++ server/app.py
@@ -0,0 +1,13 @@
+# install dependencies
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Welcome to Server Side.</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+

af9245502b4c87d2bf4d8c20b905978c79c6f423
yash264 <yp20222068@gmail.com>
2025-03-27 17:23:52 +0530

    Added Home UI

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import SparklesPreview from "../Components/Header";
+import StickyScrollReveal from "../Components/Features";
 
 function Home() {
     return (
         <>
             <Navbar />
+            <SparklesPreview />
+            <StickyScrollReveal />
         </>
     )
 }
